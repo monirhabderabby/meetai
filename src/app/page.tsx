772cd5e63c3d8489862d8114414d98283e2ddbe7
Monir Hossain <monirhrabby.personal@gmail.com>
2025-06-11 10:50:44 +0600

    Add authentication setup with better-auth and update database schema
    - Integrated better-auth for user authentication.
    - Created auth client and route for handling authentication requests.
    - Updated user and session tables in the database schema.
    - Enhanced the main page with user registration functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,60 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return <Button>Get Started</Button>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch, //refetch the session
+  } = authClient.useSession();
+
+  console.log(session);
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("success");
+        },
+      }
+    );
+  };
+  return (
+    <div className="p-4 flex flex-col gap-y-4  ">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 }
