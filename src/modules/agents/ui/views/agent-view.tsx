b104a60a2d42d57e3ecd51e503c9591eed55c3b9
Monir Hossain <monirhrabby.personal@gmail.com>
2025-06-13 21:54:34 +0600

    feat: add empty state component and integrate with agents view; update package dependencies

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import EmptyState from "@/components/empty-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { agentsColumn } from "../components/column";
+import { DataTable } from "../components/data.table";
 
 const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8  flex-col gap-y-4">
+      <DataTable data={data} columns={agentsColumn} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to join your meetings. Each agent will follow your instruction and can interact with participants during call."
+        />
+      )}
+    </div>
+  );
 };
 
 export default AgentsView;
