3e7990bc6cf97870471d67c6042647f3d46d0daf
Monir Hossain <monirhrabby.personal@gmail.com>
2025-06-14 22:27:09 +0600

    feat: implement agent detail view with loading and error states; enhance agents list with row click navigation

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -3,6 +3,7 @@
 import EmptyState from "@/components/empty-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useAgentFilter } from "../../hooks/use-agent-filter";
 import { agentsColumn } from "../components/column";
 import DataPagination from "../components/data-pagination";
@@ -10,6 +11,7 @@ import { DataTable } from "../components/data.table";
 
 const AgentsView = () => {
   const trpc = useTRPC();
+  const router = useRouter();
   const [filters, setFilters] = useAgentFilter();
   const { data } = useSuspenseQuery(
     trpc.agents.getMany.queryOptions({ ...filters })
@@ -17,7 +19,11 @@ const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8  flex-col gap-y-4">
-      <DataTable data={data.items} columns={agentsColumn} />
+      <DataTable
+        data={data.items}
+        columns={agentsColumn}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
