54265da992d5bf38c66295822daba77d0fff8b93
Monir Hossain <monirhrabby.personal@gmail.com>
2025-06-15 15:36:57 +0600

    feat: add humanize-duration for meeting duration formatting; implement DataTable component for meetings view

--- src/modules/meetings/ui/view/meeting-view.tsx
+++ src/modules/meetings/ui/view/meeting-view.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import EmptyState from "@/components/empty-state";
 import ErrorState from "@/components/error-state";
 import LoadingState from "@/components/loading";
+import { DataTable } from "@/components/ui/datatable";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
+import { meetingColumn } from "../components/column";
 
 const MeetingView = () => {
   const trpc = useTRPC();
   const { data } = useQuery(trpc.meetings.getMany.queryOptions({}));
-  return <div>Data Table {JSON.stringify(data)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data?.items ?? []} columns={meetingColumn} />
+
+      {data?.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Create an meeting to join your meetings. Each meeting lets you collaborate, share ideas, and interact with participants in real time"
+        />
+      )}
+    </div>
+  );
 };
 
 export default MeetingView;
